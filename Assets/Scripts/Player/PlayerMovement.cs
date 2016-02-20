52bf4056c41b85685cadc9e3736b20967438d2f0
Sofakinglazy <yx5m14@soton.ac.uk>
2016-02-20 01:06:23 +0000

    Done Player Movement Part

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    private Vector3 movement;
+    private Animator animator;
+    private Rigidbody rb;
+    private int floorMask;
+    private float camRayLength = 100f;
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Move(horizontal, vertical);
+        Turn();
+        Animating(horizontal, vertical);
+
+    }
+
+    void Move(float horizontal, float vertical)
+    {
+        movement.Set(horizontal, 0f, vertical);
+        movement = movement.normalized * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
+    }
+
+    void Turn()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            rb.MoveRotation(newRotation);
+        }
+    }
+
+    void Animating(float horizontal, float vertical)
+    {
+        bool walking = horizontal != 0 || vertical != 0;
+        animator.SetBool("IsWalking", walking);
+    }
+}
